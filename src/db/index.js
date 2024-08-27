90dc262446bc8f1186825ec7c25feac9388c2c7a
Aritra Nag <aritranag8835@gmail.com>
2024-08-28 03:26:23 +0530

    error in databse fixed and route 1 made

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,23 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
+
+
+
 const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
+
+
+/* const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log(`\n mongodb connected succesfully: ${connectionInstance.connection.host}`);
@@ -15,4 +31,4 @@ const connectDB = async () => {
 }
 
 
-export default connectDB
+export default connectDB */
